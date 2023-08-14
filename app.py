6323590fa7b7eaee3fa68a2b9257d363e8cca0b0
SharmajiKabetaDevesh <124495349+SharmajiKabetaDevesh@users.noreply.github.com>
2023-08-14 10:54:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+from pandas_datareader import data as pdr
+from keras.models import load_model
+import yfinance as yf    
+
+
+import streamlit as st
+
+
+
+
+yf.pdr_override()     
+start = "2010-01-01"
+end = "2023-07-30"
+
+
+
+st.title("STock Trend Prediction")
+
+user_input =st.text_input("Enter STock Ticker",'AAPL')
+df = pdr.get_data_yahoo(user_input, start, end)
+
+#describing Data
+
+st.subheader('Data from 2010 - 2023')
+st.write(df.describe())                                                                                   
