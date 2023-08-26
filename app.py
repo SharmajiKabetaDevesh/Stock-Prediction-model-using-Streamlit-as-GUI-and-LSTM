4ccb48dd4df3bd79c159013ec0286ece0b49a6a5
SharmajiKabetaDevesh <124495349+SharmajiKabetaDevesh@users.noreply.github.com>
2023-08-26 16:49:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,46 @@ plt.ylabel('Price')
 plt.legend()
 st.pyplot(fig2)  # Displays the plot in Streamlit
 
+# ... (previous code remains the same)
+
+# Getting the last 60 days of the original data for continuation
+last_60_days = data_training[-60:].values
+
+# Rescaling the data
+last_60_days_scaled = scaler.transform(last_60_days)
+
+# Creating an empty list for holding future predicted data
+X_test = []
+
+# Appending the last 60 days of scaled data to X_test
+X_test.append(last_60_days_scaled)
+
+# Converting X_test to a numpy array
+X_test = np.array(X_test)
+
+# Predicting the future stock prices
+predicted_price = model.predict(X_test)
+
+# Rescaling the predicted price
+predicted_price = predicted_price * scaler[0]
+
+# Creating a new dataframe for the future 2 days
+future_dates = pd.date_range(start=data_testing.index[-1], periods=2)
+future_df = pd.DataFrame(index=future_dates, columns=df.columns)
+
+# Adding the predicted prices to the dataframe
+future_df['Close'] = predicted_price
+
+# Plotting the graph for original data and predicted data for the future 2 days
+st.subheader('Predicted Price for the Next 2 Days')
+fig3 = plt.figure(figsize=(12, 6))
+plt.plot(data_testing.index, data_testing.values, 'b', label='Original Price')
+plt.plot(future_df.index, future_df['Close'], 'r', label='Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig3)  # Displays the plot in Streamlit
+
+
 
 #Please note that the code refers to a model named 'keras_modelv2.h5' and relies on libraries like Keras, Pandas, and Streamlit. The main purpose of this code is to fetch stock data, preprocess it, use a pre-trained model to predict future stock prices, and display the results using Streamlit.
